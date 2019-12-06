d543d596a527dd542c7f5f64066f9cbfde870832
younu8084 <55754022+younu8084@users.noreply.github.com>
2019-12-06 19:06:49 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
-#sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
-#sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-#sudo yum install jenkins
-#sudo service jenkins start
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum install jenkins
+sudo service jenkins start
 sed -i 's/port="8080"/port="8990"/' cat /etc/sysconfig/jenkins
 sudo service jenkins restart
