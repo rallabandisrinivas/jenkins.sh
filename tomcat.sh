f57babbf76cc5d80fb3479d53d5f201ea6579688
younu8084 <55754022+younu8084@users.noreply.github.com>
2019-12-09 16:02:38 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,2 +1,15 @@
-wget http://mirrors.estointernet.in/apache/tomcat/tomcat-9/v9.0.29/bin/apache-tomcat-9.0.29.tar.gz
-tar -xvzf apache-tomcat-9.0.29.tar.gz
+wget http://apachemirror.wuchna.com/tomcat/tomcat-8/v8.5.49/bin/apache-tomcat-8.5.49.tar.gz
+tar -xvzf apache-tomcat-8.5.49.tar.gz
+tar -xvzf apache-tomcat-8.5.49.tar.gz
+sed -i 's/port="8080"/port="8880"/' apache-tomcat-8.5.49/conf/server.xml
+sed -i 's\</tomcat-users>\<!-- -->\g' apache-tomcat-8.5.49/conf/tomcat-users.xml
+
+echo '<role rolename="manager-gui" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '<user username="admin" password="admin" roles="manager-gui" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '<role rolename="manager-script" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '<user username="script" password="script" roles="manager-script" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '</tomcat-users>' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+sed -i 's/<Valve /<!-- <Valve /' apache-tomcat-8.5.49/webapps/manager/META-INF/context.xml
+
+sed -i 's\:1" />\:1" /> -->\g' apache-tomcat-8.5.49/webapps/manager/META-INF/context.xml
+apache-tomcat-8.5.49/bin/./startup.sh
