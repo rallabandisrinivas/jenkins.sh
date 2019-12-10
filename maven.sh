fb5e89c690cdc3542167684cd12e344628b73dbd
younu8084 <55754022+younu8084@users.noreply.github.com>
2019-12-10 12:28:45 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,6 +1,7 @@
-sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+#sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
 
-sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-
-sudo yum install -y apache-maven
+#sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+http://mirrors.estointernet.in/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+tar xzvf apache-maven-3.6.3-bin.tar.gz
+sudo yum install -y apache-maven-3.6.3
 mvn –v
